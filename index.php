10b5bfc6786ae5b864b62b47dad2b74b69ee4aca
MiraeNassr <miraenassr@gmail.com>
2026-05-07 14:08:21 +0200

    Insert the first user and fex spelling error

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <?php 
 
 require_once 'core/database.php';
-require_once ' app/models/User.php';
+require_once 'app/models/User.php';
 
 $database = new Database();
 $db_conn = $database->getConnection();
 
 // Add copy from the model and pass it to connection
 $userModel = new UserModel($db_conn);
+if ($userModel->createUser('Nassr@gmail.com', '123456', 'admin')) {
+    echo "<br> the user add it well";
+    } else {
+        echo"error can niet add the user";
+    }
 $users = $userModel->getAllUsers();
 
 echo "<pre>";
