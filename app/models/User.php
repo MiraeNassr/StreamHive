428087f37f6c4dbd59dd03ac5d199fabf6f04de9
MiraeNassr <miraenassr@gmail.com>
2026-05-07 13:08:39 +0200

    Add user model and with query function.

---
+++ app/models/User.php
@@ -0,0 +1,23 @@
+<?php 
+
+class UserModel {
+    private $db;
+    private $table = "users";
+
+   
+// connection with database 
+    public function __construct($db_connection) {
+        $this->db = $db_connection;
+    }
+
+    // function for bring all the users in users table
+
+    public function getAllUsers() {
+        $query = "SELECT * FROM " . $this->table;
+        $stmt = $this->db->prpare($query);
+        $stmt->execute();
+   
+    // objecgt to bring the data in database
+    return $stmt->fetchAll();
+    }
+}
