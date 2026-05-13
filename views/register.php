14d0d1a378d300bd9a0e39134c9f49813707cc29
MiraeNassr <miraenassr@gmail.com>
2026-05-13 10:19:00 +0200

    Create registration form view

---
+++ views/register.php
@@ -0,0 +1,16 @@
+<h2>Reigesteren</h2>
+<!--User method post to send save data -->
+<Form action="index.php?action=register" method="post">
+    <div>
+        <label for="email">Email:</label>
+        <input type="email" name="email" required>
+    </div>
+    <div>
+        <label for="password"></label>
+        <input type="password" name="password" required>
+    </div>
+    <div>
+        <button type="submit">Account aanmaken</button>
+    </div>
+
+</Form>
