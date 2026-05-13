71f9137a5c4eec8f0be3e7b14d93f855965ccaf3
MiraeNassr <miraenassr@gmail.com>
2026-05-13 11:07:31 +0200

    Managing the user controls form

---
+++ app/controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+class UserController {
+ private $userModel;
+
+ public function __construct($userModel){
+  
+    $this->userModel = $userModel;
+ }
+ public function register() {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        if ($this->userModel->register($email, $password)){
+            echo "Registration successful";
+        }else{
+            echo "Error in register";
+        }
+    }else{
+        require_once 'app/views/register.php';
+
+    }
+        }
+    }
+ 
