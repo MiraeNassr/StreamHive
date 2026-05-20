a395088813e769812dd12b0ffdabd8ea1a704180
MiraeNassr <miraenassr@gmail.com>
2026-05-20 08:46:48 +0200

    Add new login form to the page

---
+++ views/login.php
@@ -0,0 +1,13 @@
+<h2>Inloggen</h2>
+<form action="index.php?action=login" method="POST">
+    <div>
+        <label>Email:</label>
+        <input type="email" name="email" required>
+    </div>
+    <div>
+        <label>Wachtwoord:</label>
+        <input type="password" name="password" required>
+    </div>
+    <button type="submit">Inloggen</button>
+</form>
+<p>Nog geen account? <a href="index.php?action=register">Registreer hier</a></p>
