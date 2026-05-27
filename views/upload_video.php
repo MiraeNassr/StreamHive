6fd22cadef386339d3996c39ea29c1ff42cd461e
MiraeNassr <miraenassr@gmail.com>
2026-05-27 08:50:30 +0200

    Create video upload form form

---
+++ views/upload_video.php
@@ -0,0 +1,18 @@
+<h2> Video uploaden</h2>
+
+<form action="index.php?action=upload" method="post" enctype="multipart/form-data">
+    <div>
+        <label for="title">Video Title</label>
+        <input type="text" name="title" required>
+    </div>
+
+    <div>
+        <label for="description">Beschrijving</label>
+        <textarea name="description" required></textarea>
+    </div>
+
+    <button type="submit">Video uploaden</button>
+
+</form>
+
+<p><a href="index.php">Terug naar Homepage</a></p>
