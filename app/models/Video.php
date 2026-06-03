5c6a484543ea3ddcac9e16196ea0a8a8e524d9c7
MiraeNassr <miraenassr@gmail.com>
2026-06-03 10:20:56 +0200

    Fix Query and some syntax error

--- app/models/Video.php
+++ app/models/Video.php
@@ -12,30 +12,23 @@ class VideoModel {
     //function for bring all the videos with the users name
     public function getAllVideos(){
         // this is to know wich user push it the video
-        $query = "SELECT videos.*, users.email 
-                  FROM " . $this->table . " 
-                  JOIN users ON videos.user_id = users.id 
-                  ORDER BY videos.created_at DESC";// DESC it to show the videos from new to old
-
+        $query = "SELECT * FROM videos ORDER BY id DESC" ;
         $stmt = $this->db->prepare($query);
         $stmt->execute();
         
-        return $stmt->fetchAll();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC); // return the videos like an array
     }
 
-    public function createVideo($title, $description, $filename, $user_id) {
-        $query = "INSERT INTO " . $this->table . " (user_id, title, description, filename) VALUES (:user_id, :title, :description, :filename)";
-
+   public function createVideo($title, $description, $filename, $user_id) {
+        $query = "INSERT INTO " . $this->table . " (title, description, filename, user_id) 
+                  VALUES (:title, :description, :filename, :user_id)";
+        
         $stmt = $this->db->prepare($query);
-
-        $stmt->bindParam(':user_id', $user_id);
         $stmt->bindParam(':title', $title);
         $stmt->bindParam(':description', $description);
         $stmt->bindParam(':filename', $filename);
+        $stmt->bindParam(':user_id', $user_id);
 
-        if($stmt->execute()) {
-            return true;
-        }
-        return false;
-    }
+        return $stmt->execute();
+   }
 }
