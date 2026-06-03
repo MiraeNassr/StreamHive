72e4bdb9622c25df8620ee603fd79b6594b8d9d0
MiraeNassr <miraenassr@gmail.com>
2026-06-03 10:18:06 +0200

    Add new div for choice video from the device

--- views/upload_video.php
+++ views/upload_video.php
@@ -1,6 +1,6 @@
 <h2> Video uploaden</h2>
 
-<form action="index.php?action=upload" method="post" enctype="multipart/form-data">
+<form action="./index.php?action=upload" method="post" enctype="multipart/form-data">
     <div>
         <label for="title">Video Title</label>
         <input type="text" name="title" required>
@@ -11,6 +11,10 @@
         <textarea name="description" required></textarea>
     </div>
 
+    <div>
+        <label for="video_file">Kies video bestand</label>
+        <input type="file" name="video_file" accept="video/*" required>
+    </div>
     <button type="submit">Video uploaden</button>
 
 </form>
