6ae65b1734474c4f2628d2c57c9ab6bafe14ce39
MiraeNassr <miraenassr@gmail.com>
2026-06-10 10:29:44 +0200

    Add new functions to comment.php: addComment(), getCommentByVideoId()

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -7,4 +7,31 @@ class CommentModel {
     public function __construct($db) {
         $this->db = $db;
     }
+//function to save the comments in database
+    public function addComment($video_id, $user_id, $comment_text) {
+        $query = "INSERT INTO " . $this->table . " (video_id, user_id, comment_text) 
+                  VALUES (:video_id, :user_id, :comment_text)";
+        
+        $stmt = $this->db->prepare($query);
+        $stmt->bindParam(':video_id', $video_id);
+        $stmt->bindParam(':user_id', $user_id);
+        $stmt->bindParam(':comment_text', $comment_text);
+        
+        return $stmt->execute();
+    }
+
+    // function to cupel the user with his comment
+
+    public function getCommentsByVideoId($video_id) {
+        $query = "SELECT c.*, u.email FROM " . $this->table . " c 
+                  JOIN users u ON c.user_id = u.id 
+                  WHERE c.video_id = :video_id 
+                  ORDER BY c.created_at DESC";
+                  
+        $stmt = $this->db->prepare($query);
+        $stmt->bindParam(':video_id', $video_id);
+        $stmt->execute();
+        
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
