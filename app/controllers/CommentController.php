0390ccfa51d2250d777d49c6ec01bf39d309ead2
MiraeNassr <miraenassr@gmail.com>
2026-06-10 10:34:35 +0200

    Add new function to add new comment function store()

--- app/controllers/CommentController.php
+++ app/controllers/CommentController.php
@@ -1,7 +1,15 @@
+<?php
 class CommentController {
 private $commentModel;
 
 public function __construct($commentModel) {
 $this->commentModel = $commentModel;
 }
+//Add new comment
+public function store() {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
+    }    
 }
